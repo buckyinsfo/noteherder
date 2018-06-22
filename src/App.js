eddac5411270f09e864c6218ab7644dbb8cc408e
buckyinsfo <buckyinsfo@gmail.com>
2018-06-21 23:25:21 -0700

    Initial add.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const i = 0
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Noteherder
+      </div>
+    );
+  }
+}
+
+export default App;
