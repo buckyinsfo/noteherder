a2e6c973ec47908e1960eb3098f155652656dcb7
buckyinsfo <buckyinsfo@gmail.com>
2018-06-30 00:53:26 -0700

    Style Sidebar using aphrodite.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
 
 import quill from './assets/img/quill.svg'
@@ -10,19 +11,19 @@ import newHover from './assets/img/new-hover.png'
 const Sidebar = () => {
 
     return (
-        <div className="Sidebar" style={styles.sidebar}>
-            <div className="logo" style={styles.logo}>
-                <img src={quill} alt="Noteherder" style={styles.logoImg} />
+        <div className={ css( styles.sidebar ) } >
+            <div className={ css( styles.logo ) }>
+                <img src={quill} alt="Noteherder" className={ css( styles.logoImg ) } />
             </div>
 
-            <a href="./">
-                <img src={newIcon} alt="New note" />
-                <img src={newHover} alt="New note" />
+            <a href="/notes" className={ css( styles.newNote ) } >
+                <img src={newHover} alt="New note" className={ css( styles.newNoteImg ) } />
+                <img src={newIcon} alt="New note hover" className={ css( styles.newNoteImg, styles.newNoteImgHover ) } />
             </a>
 
             <div className="SignOut">
                 <button>
-                    <i className="fa fa-sign-out">
+                    <i className={`fa fa-sign-out ${ css( styles.buttonIcon )}` }>
                     </i>
                 </button>
             </div>
@@ -31,7 +32,7 @@ const Sidebar = () => {
 
 }
 
-const styles = {
+const styles = StyleSheet.create({
     sidebar: {
         width: '6rem',
         backgroundColor: '#f3f3f3',
@@ -48,7 +49,23 @@ const styles = {
     logoImg: {
         width: '3rem',
         paddingLeft: '0.4rem',
-    }
-}
+    },
+    newNote: {
+        marginTop: '2rem',
+        position: 'relative',
+        width: '4rem',
+    },
+    newNoteImg: {
+        position: 'absolute',
+        left: '0',
+        width: '100%',
+        transition: 'opacity 0.25s ease-in-out',
+    },
+    newNoteImgHover: {
+        ':hover': {
+            opacity: 0,
+        },
+    },
+})
 
 export default Sidebar;
