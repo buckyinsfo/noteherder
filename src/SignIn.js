2e5bdaf5474c74bb5a327436cbdb3c157af2aa51
buckyinsfo <buckyinsfo@gmail.com>
2018-07-10 15:13:48 -0700

    Style SignIn page.

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './SignIn.css'
+import googleLogo from './assets/img/google.svg'
 
 const SignIn = ({ handleAuth }) => {
     return (
@@ -11,11 +13,11 @@ const SignIn = ({ handleAuth }) => {
                 <h3>Hey, Nerd! You Like Notes?</h3>
                 <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
                 <button className="github" onClick={handleAuth} >
-                    <i className="fa fa-github"></i>
+                    <i className="fab fa-github"></i>
                     Sign in with GitHub
                 </button>
-                <button className="google"> >
-                    <img src="media/google.svg" alt="" />
+                <button className="google">
+                    <img src={googleLogo} alt="" />
                     Sign in with Google
                 </button>
             </main>
