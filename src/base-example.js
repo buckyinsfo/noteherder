bb2af573bc20f23b65b8bc423a8ceb826a428aef
buckyinsfo <buckyinsfo@gmail.com>
2018-07-15 02:15:41 -0700

    Update re-base config example.

--- src/base-example.js
+++ src/base-example.js
@@ -1,11 +1,12 @@
 // Copy this file to base.js and add your app details.
 
 import firebase from 'firebase/app'
-import  'firebase/database'
+import 'firebase/database'
+import 'firebase/auth'
+
 import Rebase from 're-base'
 
 // Initialize Firebase
-
 const config = {
     apiKey: "YOUR API KEY",
     authDomain: "YOUR AUTH DOIMAIN",
@@ -15,7 +16,11 @@ const config = {
     messagingSenderId: "YOUR MESSAGE SENDER ID"
   };
   
- const app = firebase.initializeApp(config);
- const db = firebase.database(app)
-
- export default Rebase.createClass(db)
+  const app = firebase.initializeApp(config)
+  const db = firebase.database(app)
+ 
+  export const githubProvider = new firebase.auth.GithubAuthProvider()
+  export const googleProvider = new firebase.auth.GoogleAuthProvider()
+  export const auth = app.auth()
+ 
+  export default Rebase.createClass(db)
